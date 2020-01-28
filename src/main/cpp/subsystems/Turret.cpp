2acc0ef5a2e40b331c859b6643d49751c352c2b5
Adan Silva <ev3mcdesigner@gmail.com>
2020-01-27 20:04:17 -0800

    Add turret subsystem

--- src/main/cpp/subsystems/Turret.cpp
+++ src/main/cpp/subsystems/Turret.cpp
@@ -3,3 +3,17 @@
 #include "subsystems/Turret.hpp"
 
 using namespace frc3512;
+using namespace frc3512::Constants::Turret;
+
+Turret::Turret() {
+    m_motor.Set(0);
+    ResetEncoder();
+}
+
+void Turret::SetVoltage(units::volt_t voltage) { m_motor.SetVoltage(voltage); }
+
+void Turret::ResetEncoder() { m_encoder.Reset(); }
+
+bool Turret::GetLeftHallTriggered() const { return !m_leftHall.Get(); }
+
+bool Turret::GetRightHallTriggered() const { return !m_rightHall.Get(); }
--- src/main/include/subsystems/Turret.hpp
+++ src/main/include/subsystems/Turret.hpp
@@ -2,15 +2,63 @@
 
 #pragma once
 
+#include <limits>
+
+#include <frc/DigitalInput.h>
+#include <frc/Encoder.h>
+#include <rev/CANSparkMax.h>
+
 #include "Constants.hpp"
 #include "communications/PublishNode.hpp"
 #include "subsystems/SubsystemBase.hpp"
 
 namespace frc3512 {
 
+/**
+ *  State class which stores numerical values as "states" for the Turret to use.
+ */
+enum TurretState { kIDLE = 0, kMostLeft, KMostRight };
+
+/**
+ * Subsystem specifically designed for the Turret (bottom, movable part of the
+ * Shooter)
+ */
 class Turret : public SubsystemBase, public PublishNode {
 public:
-private:
-};
+    Turret();
+    Turret(const Turret&) = delete;
+    Turret& operator=(const Turret&) = delete;
 
+    /**
+     *  Set the velocity of the Spark Max, which is wired to the Turret.
+     *
+     *  Can be seen as a way of having it turn in a certain direction.
+     *  This depends on the value given to it.
+     *
+     *  @param velocity between [-1.0 ... 1.0]
+     */
+    void SetVoltage(units::volt_t voltage);
+
+    /**
+     *  Resets the encoder wired to the Turret.
+     */
+    void ResetEncoder();
+
+    bool GetLeftHallTriggered() const;
+
+    bool GetRightHallTriggered() const;
+
+private:
+    // Encoder
+    frc::Encoder m_encoder{Constants::Turret::kEncoderA,
+                           Constants::Turret::kEncoderB};
+
+    // Both hall sensors for each boundary of the turret
+    frc::DigitalInput m_rightHall{Constants::Turret::kRightHallPort};
+    frc::DigitalInput m_leftHall{Constants::Turret::kLeftHallPort};
+
+    // Spark Max
+    rev::CANSparkMax m_motor{Constants::Turret::kPort,
+                             rev::CANSparkMax::MotorType::kBrushless};
+};
 }  // namespace frc3512
