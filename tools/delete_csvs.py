ce6a052923c56ef12f579ffe7d58767ef24107f9
Tyler Veness <calcmogul@gmail.com>
2020-02-05 21:26:49 -0800

    Add --all flag to delete_csvs.py

--- tools/delete_csvs.py
+++ tools/delete_csvs.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 """Deletes all but newest versions of CSV log files."""
 
+import argparse
 import os
 import re
 import subprocess
+
+parser = argparse.ArgumentParser(description="Deletes CSVs from the roboRIO")
+parser.add_argument(
+    "--all",
+    action="store_true",
+    help="if specified, deletes all CSV files instead of leaving the most recent ones.",
+)
+args = parser.parse_args()
 
 # Get list of files in current directory
 files = (
@@ -12,7 +21,7 @@ files = (
     .split("\n")
 )
 
-# Maps subsystem name to tuple of csv_group and date and filters for CSV files.
+# Maps subsystem name to tuple of csv_group and date and filters for CSV files
 filtered = {}
 file_rgx = re.compile(
     r"^(?P<name>[A-Za-z ]+)-(?P<date>\d{4}-\d{2}-\d{2}-\d{2}_\d{2}_\d{2})\.csv$"
@@ -27,13 +36,16 @@ for f in files:
     if name not in filtered or filtered[name] < name:
         filtered[name] = f
 
-# Makes list of files to delete.
+# Makes list of files to delete
 csvs_to_delete = []
-for f in files:
-    match = file_rgx.search(f)
-    name = match.group("name")
-    if filtered[name] != f:
-        csvs_to_delete.append(f)
+if args.all:
+    csvs_to_delete = files
+else:
+    for f in files:
+        match = file_rgx.search(f)
+        name = match.group("name")
+        if filtered[name] != f:
+            csvs_to_delete.append(f)
 
 # Delete list of files from roboRIO
 subprocess.run(["ssh", "lvuser@10.35.12.2", "rm", "-f", " ".join(csvs_to_delete)])
