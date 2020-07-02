7d2a52261335d10ce18608963e15835bfdcf560b
Tyler Veness <calcmogul@gmail.com>
2020-07-01 23:33:29 -0700

    Fix old test log file removal

--- gradlew-test.sh
+++ gradlew-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Remove old log files
-rm -f *.csv Robot.log
+rm -f build/test-results/frcUserProgramTest/linuxx86-64/release/{*.csv,Robot.log}
 
 ./gradlew runFrcUserProgramTestLinuxx86-64ReleaseGoogleTestExe
