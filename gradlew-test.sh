9137a199d21582b9d78ffa6ff4fb63d9f4943bad
Tyler Veness <calcmogul@gmail.com>
2020-09-03 09:40:23 -0700

    Add argument passthrough to gradlew wrapper scripts

--- gradlew-test.sh
+++ gradlew-test.sh
@@ -3,4 +3,4 @@
 # Remove old log files
 rm -f build/test-results/frcUserProgramTest/linuxx86-64/release/{*.csv,Robot.log}
 
-./gradlew runFrcUserProgramTestLinuxx86-64ReleaseGoogleTestExe
+./gradlew runFrcUserProgramTestLinuxx86-64ReleaseGoogleTestExe $@
