f98870a9466f01401a3611629ed623403404b34f
Tyler Veness <calcmogul@gmail.com>
2020-09-02 21:34:00 -0700

    Refactor drivetrain simulation into a function
    This allowed more easily adding a unit test for a different trajectory.

--- src/test/cpp/DrivetrainControllerTest.cpp
+++ src/test/cpp/DrivetrainControllerTest.cpp
@@ -14,71 +14,51 @@
 
 static constexpr bool kIdealModel = false;
 
-TEST(DrivetrainControllerTest, ReachesReference) {
+void RunSimulation(
+    frc3512::DrivetrainController& controller,
+    Eigen::Matrix<double, 10, 1> x = Eigen::Matrix<double, 10, 1>::Zero()) {
     using frc3512::Constants::kDt;
 
-    frc::sim::RoboRioSim roboRIO{0};
-
-    frc::Pose2d initialPose{12.65_m, 5.800_m - 0.343_m,
-                            units::radian_t{wpi::math::pi}};
-
-    frc3512::DrivetrainController controller;
+    frc::Pose2d initialPose{
+        units::meter_t{x(frc3512::DrivetrainController::State::kX)},
+        units::meter_t{x(frc3512::DrivetrainController::State::kY)},
+        units::radian_t{x(frc3512::DrivetrainController::State::kHeading)}};
     controller.Reset(initialPose, initialPose);
-    controller.SetOpenLoop(false);
-    controller.Enable();
-
-    Eigen::Matrix<double, 10, 1> x = Eigen::Matrix<double, 10, 1>::Zero();
-    x(frc3512::DrivetrainController::State::kX) =
-        initialPose.Translation().X().to<double>();
-    x(frc3512::DrivetrainController::State::kY) =
-        initialPose.Translation().Y().to<double>();
-    x(frc3512::DrivetrainController::State::kHeading) =
-        initialPose.Rotation().Radians().to<double>();
 
     Eigen::Matrix<double, 2, 1> u = Eigen::Matrix<double, 2, 1>::Zero();
 
+    frc::sim::RoboRioSim roboRIO{0};
+
     auto currentTime = 0_s;
-
-    controller.Update(kDt, currentTime);
-
-    controller.SetWaypoints(
-        frc::Pose2d(12.65_m, 5.800_m - 0.343_m, units::radian_t{wpi::math::pi}),
-        {},
-        frc::Pose2d(12.65_m - 0.9398_m - 0.5_m, 5.800_m - 0.343_m,
-                    units::radian_t{wpi::math::pi}));
     while (currentTime < 10_s) {
         auto dt = kDt;
-
-        // Add scheduling jitter
         if constexpr (!kIdealModel) {
             dt += units::second_t{frc::MakeWhiteNoiseVector(0.001)(0)};
         }
 
+        // Update drivetrain controller
         Eigen::Matrix<double, 3, 1> y =
             frc3512::DrivetrainController::LocalMeasurementModel(
                 x, Eigen::Matrix<double, 2, 1>::Zero());
-
-        // Add measurement noise
+        // Add measurement noise to drivetrain controller
         if constexpr (!kIdealModel) {
             y += frc::MakeWhiteNoiseVector(0.0001, 0.005, 0.005);
         }
-
         controller.SetMeasuredLocalOutputs(
             units::radian_t{y(0)}, units::meter_t{y(1)}, units::meter_t{y(2)});
-        controller.SetMeasuredInputs(units::volt_t{u(0)}, units::volt_t{u(1)});
         controller.Update(kDt, currentTime);
 
         u = controller.GetInputs();
 
-        // Account for battery voltage drop due to current draw
+        // Account for battery voltage drop due to current draw for the turret
         if constexpr (!kIdealModel) {
             constexpr auto Vbat = 12_V;
             constexpr auto Rbat = 0.03_Ohm;
-            constexpr auto r = 0.0746125_m;  // Wheel radius
+            constexpr auto r = 0.0746125_m;  // Drivetrain wheel radius
 
             using Input = frc3512::DrivetrainController::Input;
             using State = frc3512::DrivetrainController::State;
-            constexpr auto motors = frc::DCMotor::MiniCIM(3);
+            constexpr auto motors = frc::DCMotor::NEO(2);
             units::ampere_t loadIleft =
                 motors.Current(
                     units::meters_per_second_t{x(State::kLeftVelocity)} / r *
@@ -102,8 +82,51 @@ TEST(DrivetrainControllerTest, ReachesReference) {
         x = frc::RungeKutta(frc3512::DrivetrainController::Dynamics, x, u, dt);
         currentTime += dt;
     }
+}
 
-    RenameCSVs("DrivetrainControllerTest", "./Drivetrain ");
+TEST(DrivetrainControllerTest, ReachesReferenceStraight) {
+    using frc3512::Constants::kDt;
+
+    frc::sim::RoboRioSim roboRIO{0};
+
+    frc::Pose2d initialPose{12.65_m, 5.800_m - 0.343_m,
+                            units::radian_t{wpi::math::pi}};
+
+    Eigen::Matrix<double, 10, 1> x0 = Eigen::Matrix<double, 10, 1>::Zero();
+    x0(frc3512::DrivetrainController::State::kX) =
+        initialPose.Translation().X().to<double>();
+    x0(frc3512::DrivetrainController::State::kY) =
+        initialPose.Translation().Y().to<double>();
+    x0(frc3512::DrivetrainController::State::kHeading) =
+        initialPose.Rotation().Radians().to<double>();
+
+    frc3512::DrivetrainController controller;
+    controller.SetWaypoints(
+        initialPose, {},
+        frc::Pose2d(12.65_m - 0.9398_m - 0.5_m, 5.800_m - 0.343_m,
+                    units::radian_t{wpi::math::pi}));
+    controller.Enable();
+
+    RunSimulation(controller, x0);
+
+    RenameCSVs("DrivetrainControllerTest Straight", "./Drivetrain ");
+
+    EXPECT_TRUE(controller.AtGoal());
+}
+
+TEST(DrivetrainControllerTest, ReachesReferenceCurve) {
+    using frc3512::Constants::kDt;
+
+    frc::sim::RoboRioSim roboRIO{0};
+
+    frc3512::DrivetrainController controller;
+    controller.SetWaypoints(frc::Pose2d(0_m, 0_m, 0_rad), {},
+                            frc::Pose2d(4.8768_m, 2.7432_m, 0_rad));
+    controller.Enable();
+
+    RunSimulation(controller);
+
+    RenameCSVs("DrivetrainControllerTest Curve", "./Drivetrain ");
 
     EXPECT_TRUE(controller.AtGoal());
 }
