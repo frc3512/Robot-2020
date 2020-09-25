7f8a8ea8071ddaa8e847db50d68a2c64041aa887
Tyler Veness <calcmogul@gmail.com>
2020-09-25 11:08:15 -0700

    Elaborate on StepTimingToNextNotifier() docs

--- src/test/include/TestUtil.hpp
+++ src/test/include/TestUtil.hpp
@@ -9,6 +9,8 @@ namespace frc3512::sim {
 /**
  * Steps simulation timing to when the next Notifier is scheduled.
  *
+ * If there is no Notifier scheduled, simulation timing is left unchanged.
+ *
  * @param delta An extra time delta that can be used for injecting scheduling
  *              jitter. This should be greater than zero so the notifier still
  *              triggers.
