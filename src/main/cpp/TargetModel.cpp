ab8acd34dc45bb85eaa4f48240511348c726387d
KyleQ1 <kylequinlan09@gmail.com>
2020-12-05 16:08:47 -0800

    Move target model to the left and move it back

--- src/main/cpp/TargetModel.cpp
+++ src/main/cpp/TargetModel.cpp
@@ -3,5 +3,5 @@
 #include "TargetModel.hpp"
 
 namespace TargetModel {
-const frc::Translation2d kOffset{units::inch_t{-5.0}, 0_in};
+const frc::Translation2d kOffset{0_in, units::inch_t{5.0}};
 }  // namespace TargetModel
