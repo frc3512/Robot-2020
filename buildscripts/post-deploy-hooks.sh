e9c5c84d0605988c2003e035647a85439ec6e303
Tyler Veness <calcmogul@gmail.com>
2021-01-25 12:24:21 -0800

    Make postdeploy hooks run in Gradle predeploy

--- buildscripts/post-deploy-hooks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Disable crond because it uses 50% CPU and there's no cronjobs to run
-ssh admin@10.35.12.2 /etc/init.d/crond stop
-
-# Disable NI web server because it leaks memory badly
-ssh admin@10.35.12.2 /usr/local/natinst/etc/init.d/systemWebServer stop
