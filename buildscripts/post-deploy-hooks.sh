345dd383e7f4ecc1e1fc003940ae19ca531234ec
Tyler Veness <calcmogul@gmail.com>
2021-01-30 10:11:13 -0800

    Upgrade to WPILib 2021.2.2
    GradleRIO now gives RT capabilities, so we don't need to do it
    ourselves.

--- buildscripts/post-deploy-hooks.sh
+++ buildscripts/post-deploy-hooks.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Give RT capability
-ssh admin@10.35.12.2 setcap cap_sys_nice+eip /home/lvuser/frcUserProgram
-
 # Disable crond because it uses 50% CPU and there's no cronjobs to run
 ssh admin@10.35.12.2 /etc/init.d/crond stop
 
