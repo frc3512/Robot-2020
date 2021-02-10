f79840ddb1a2e45e2153c03ebc634b969d6b2212
Tyler Veness <calcmogul@gmail.com>
2021-02-09 18:00:34 -0800

    Fix LQR template type deduction on Windows

--- src/main/cpp/controllers/DrivetrainController.cpp
+++ src/main/cpp/controllers/DrivetrainController.cpp
@@ -160,7 +160,7 @@ Eigen::Matrix<double, 2, 5> DrivetrainController::ControllerGainForState(
     }
 
     m_A(State::kY, State::kHeading) = velocity;
-    return LQR(m_A, m_B, m_Q, m_R, kDt);
+    return LQR<5, 2>(m_A, m_B, m_Q, m_R, kDt);
 }
 
 Eigen::Matrix<double, 2, 1> DrivetrainController::Controller(
