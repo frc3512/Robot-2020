1ea346f83d8dae434dd47a202232966711ac1566
Tyler Veness <calcmogul@gmail.com>
2021-02-20 17:12:45 -0800

    Add waypoints to AutoNav Bounce to avoid cones

--- src/main/cpp/autonomous/AutoNavBounce.cpp
+++ src/main/cpp/autonomous/AutoNavBounce.cpp
@@ -6,30 +6,43 @@
 
 namespace frc3512 {
 void Robot::AutoNavBounce() {
+    const auto kMarkerOffset = -Drivetrain::kLength / 16.0;
+
     // Initial Pose - right up against the Start zone border.
     const frc::Pose2d kInitialPose{1.076_m, 2.276_m, units::radian_t{0}};
     // First Star - right before the first star so the robot touches up against
     // it.
-    const frc::Pose2d kFirstStar{2.303_m, 3.797_m,
+    const frc::Pose2d kFirstStar{2.303_m, 3.797_m + kMarkerOffset,
                                  units::radian_t{(wpi::math::pi) / 2}};
+    // First Side Curve 1 - the first lower curve path between the fist and
+    // second star.
+    const frc::Pose2d kFirstSideCurve1{2.7_m + 0.15_m, 2.0 * 0.806_m,
+                                       units::radian_t{wpi::math::pi / 2.0}};
     // First Bottom Curve - the first lower curve path between the fist and
     // second star.
-    const frc::Pose2d kFirstBottomCurve{3.62_m, 0.806_m,
-                                        units::radian_t{wpi::math::pi}};
+    const frc::Pose2d kFirstBottomCurve1{3.62_m - 0.3_m, 0.806_m,
+                                         units::radian_t{wpi::math::pi}};
+    // First Bottom Curve - the first lower curve path between the fist and
+    // second star.
+    const frc::Pose2d kFirstBottomCurve2{3.62_m + 0.3_m, 0.806_m,
+                                         units::radian_t{wpi::math::pi}};
+    // First Side Curve 2 - the first lower curve path between the fist and
+    // second star.
+    const frc::Pose2d kFirstSideCurve2{4.578_m, 2.0_m,
+                                       units::radian_t{3 * wpi::math::pi / 2}};
     // Second Star - right before the second star so the robot touches up
     // against it.
-    const frc::Pose2d kSecondStar{4.578_m, 3.797_m,
+    const frc::Pose2d kSecondStar{4.578_m, 3.797_m + kMarkerOffset,
                                   units::radian_t{(3 * wpi::math::pi) / 2}};
     // Second Bottom Curve - the second lower curve path between the second and
     // third star.
     const frc::Pose2d kSecondBottomCurve{5.703_m, 0.844_m, units::radian_t{0}};
     // Third Star - right before the third star so the robot touches up against
     // it.
-    const frc::Pose2d kThirdStar{6.815_m, 3.797_m,
+    const frc::Pose2d kThirdStar{6.815_m, 3.797_m + kMarkerOffset,
                                  units::radian_t{(wpi::math::pi) / 2}};
     // End Pose - right inside the Finish Zone.
-    const frc::Pose2d kEndPose{7.979_m, 2.263_m,
-                               units::radian_t{wpi::math::pi}};
+    const frc::Pose2d kEndPose{7.979_m, 2.5_m, units::radian_t{wpi::math::pi}};
 
     turret.SetControlMode(TurretController::ControlMode::kManual);
     drivetrain.Reset(kInitialPose);
@@ -39,8 +52,10 @@ void Robot::AutoNavBounce() {
     backwardConfig.SetReversed(true);
 
     drivetrain.AddTrajectory(kInitialPose, {}, kFirstStar, forwardConfig);
-    drivetrain.AddTrajectory({kFirstStar, kFirstBottomCurve, kSecondStar},
-                             backwardConfig);
+    drivetrain.AddTrajectory(
+        {kFirstStar, kFirstSideCurve1, kFirstBottomCurve1, kFirstBottomCurve2,
+         kFirstSideCurve2, kSecondStar},
+        backwardConfig);
     drivetrain.AddTrajectory({kSecondStar, kSecondBottomCurve, kThirdStar},
                              forwardConfig);
     drivetrain.AddTrajectory(kThirdStar, {}, kEndPose, backwardConfig);
