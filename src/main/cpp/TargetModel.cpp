c60b996571b456536ad332a27f35935310902fce
KyleQ1 <kylequinlan09@gmail.com>
2021-02-25 19:27:04 -0800

    Increase TargetModel offset

--- src/main/cpp/TargetModel.cpp
+++ src/main/cpp/TargetModel.cpp
@@ -1,7 +1,7 @@
-// Copyright (c) 2020 FRC Team 3512. All Rights Reserved.
+// Copyright (c) 2020-2021 FRC Team 3512. All Rights Reserved.
 
 #include "TargetModel.hpp"
 
 namespace TargetModel {
-const frc::Translation2d kOffset{0_in, units::inch_t{5.0}};
+const frc::Translation2d kOffset{0_in, 12_in};
 }  // namespace TargetModel
