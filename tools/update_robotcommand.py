4ee0e85720d43c1234e913bda280800e0fa44c64
Tyler Veness <calcmogul@gmail.com>
2021-03-06 10:38:54 -0800

    Modify robotCommand to stop unnecessary services

--- tools/update_robotcommand.py
+++ tools/update_robotcommand.py
@@ -7,13 +7,16 @@ import subprocess
 def main():
     # Stops crond and systemWebServer before starting robot program
     with open("robotCommand", "w") as output:
-        output.write(""" \"/etc/init.d/crond stop\"
- \"/usr/local/natinst/etc/init.d/systemWebServer stop\"
- \"/home/lvuser/frcUserProgram\"
+        output.write(
+            """sh -c '/etc/init.d/crond stop && /usr/local/natinst/etc/init.d/systemWebServer stop && /home/lvuser/frcUserProgram'
 """)
     subprocess.check_output(
         ["scp", "robotCommand", "lvuser@10.35.12.2:/home/lvuser"])
     os.remove("robotCommand")
+    subprocess.check_output([
+        "ssh", "lvuser@10.35.12.2",
+        ". /etc/profile.d/natinst-path.sh; /usr/local/frc/bin/frcKillRobot.sh -t -r 2> /dev/null"
+    ])
 
 
 if __name__ == "__main__":
