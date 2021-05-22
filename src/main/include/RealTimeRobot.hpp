784e18c263800619e9bf5e30c51f52192cadd660
Tyler Veness <calcmogul@gmail.com>
2021-05-21 19:53:47 -0700

    Upgrade to Doxygen 1.8.20 and fix Doxygen comments

--- src/main/include/RealTimeRobot.hpp
+++ src/main/include/RealTimeRobot.hpp
@@ -27,13 +27,14 @@ public:
     /**
      * Constructor for RealTimeRobot.
      *
-     * @param robotAllocation  The allocation to give the RealTimeRobot periodic
-     *                         functions.
-     * @param controllerPeriod The controller period. This is the basis for
-     * @param priority         Priority to set the thread to. For real-time,
-     *                         this is 1-99 with 99 being highest. For
-     *                         non-real-time, this is forced to 0. See "man 7
-     *                         sched" for more details.
+     * @param robotPeriodicAllocation The allocation to give the RealTimeRobot
+     *                                periodic functions.
+     * @param controllerPeriod The controller period. The sum of all scheduler
+     *                         allocations should be less than or equal to this
+     *                         value.
+     * @param priority Priority to set the thread to. For real-time, this is
+     *                 1-99 with 99 being highest. For non-real-time, this is
+     *                 forced to 0. See "man 7 sched" for more details.
      */
     explicit RealTimeRobot(
         units::second_t robotPeriodicAllocation = 2_ms,
