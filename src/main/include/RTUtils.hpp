278ca3cb12443d091961ab7473d2b4ee4b6e161e
Tyler Veness <calcmogul@gmail.com>
2021-07-12 13:06:11 -0700

    Fix typo in SetProcessRTPriority() docs

--- src/main/include/RTUtils.hpp
+++ src/main/include/RTUtils.hpp
@@ -10,7 +10,7 @@ namespace frc3512 {
  * Gives a currently running process the specified RT priority.
  *
  * @param process  A substring of the process name.
- * @param priority Priority to set the thread to. For real-time, this is 1-99
+ * @param priority Priority to set the process to. For real-time, this is 1-99
  *                 with 99 being highest. For non-real-time, this is forced to
  *                 0. See "man 7 sched" for more details.
  * @throw std::runtime_exception Runtime exception if setting RT priority
