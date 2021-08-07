0ea1b5aad9f11428f08b80e670699719fc959ade
Tyler Veness <calcmogul@gmail.com>
2021-08-07 14:13:43 -0700

    Use RAII wrapper for setuid()

--- src/main/cpp/RTUtils.cpp
+++ src/main/cpp/RTUtils.cpp
@@ -13,13 +13,42 @@
 #include <fmt/format.h>
 #include <frc/RobotBase.h>
 
+namespace {
+#if !defined(_MSC_VER)
+class UidSetter {
+public:
+    explicit UidSetter(uid_t uid) {
+        m_uid = getuid();
+        if (setuid(uid) == -1) {
+            throw std::system_error(errno, std::generic_category(),
+                                    fmt::format("setuid({}) failed", uid));
+        }
+    }
+
+    ~UidSetter() noexcept(false) {
+        if (setuid(m_uid) == -1) {
+            throw std::system_error(errno, std::generic_category(),
+                                    fmt::format("setuid({}) failed", m_uid));
+        }
+    }
+
+private:
+    uid_t m_uid;
+};
+#else
+class UidSetter {
+public:
+    explicit UidSetter(int uid) {}
+};
+#endif  // !defined(_MSC_VER)
+}  // namespace
+
 namespace frc3512 {
 
 void SetProcessRTPriority(std::string_view process, int priority) {
     if constexpr (!frc::RobotBase::IsSimulation()) {
-#if !defined(_MSC_VER)
-        setuid(0);
-#endif  // !defined(_MSC_VER)
+        UidSetter uidSetter{0};
+
         if (std::system(fmt::format("pgrep '{}' | xargs chrt -f -p {}", process,
                                     priority)
                             .c_str()) != 0) {
--- src/main/cpp/Setup.cpp
+++ src/main/cpp/Setup.cpp
@@ -12,14 +12,42 @@
 #include <frc/RobotBase.h>
 #include <fmt/format.h>
 
+namespace {
+#if !defined(_MSC_VER)
+class UidSetter {
+public:
+    explicit UidSetter(uid_t uid) {
+        m_uid = getuid();
+        if (setuid(uid) == -1) {
+            throw std::system_error(errno, std::generic_category(),
+                                    fmt::format("setuid({}) failed", uid));
+        }
+    }
+
+    ~UidSetter() noexcept(false) {
+        if (setuid(m_uid) == -1) {
+            throw std::system_error(errno, std::generic_category(),
+                                    fmt::format("setuid({}) failed", m_uid));
+        }
+    }
+
+private:
+    uid_t m_uid;
+};
+#else
+class UidSetter {
+public:
+    explicit UidSetter(int uid) {}
+};
+#endif  // !defined(_MSC_VER)
+}  // namespace
+
 namespace frc3512 {
 
 void StopCrond() {
     if constexpr (!frc::RobotBase::IsSimulation()) {
-        // crond occasionally uses 50% CPU and there's no cronjobs to run
-#if !defined(_MSC_VER)
-        setuid(0);
-#endif  // !defined(_MSC_VER)
+        UidSetter uidSetter{0};
+
         int status = std::system("/etc/init.d/crond stop");
         if (status != 0) {
             throw std::runtime_error(
